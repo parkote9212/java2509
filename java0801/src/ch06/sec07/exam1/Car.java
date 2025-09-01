c19284e34bd5f6c5700a124d0feffca8b557e0b9
parkote9212 <parkote9212@gmail.com>
2025-09-01 15:29:56 +0900

    java code

---
+++ java0801/src/ch06/sec07/exam1/Car.java
@@ -0,0 +1,7 @@
+package ch06.sec07.exam1;
+
+public class Car {
+    Car(String model, String color, int maxSpeed){
+
+    }
+}
---
+++ java0801/src/ch06/sec07/exam1/CarExample.java
@@ -0,0 +1,7 @@
+package ch06.sec07.exam1;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car myCar = new Car("그랜저", "검정", 250);
+    }
+}

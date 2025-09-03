692a76cd2f1f4dfd2ee4185b6a1a4dea84fbcbb6
parkote9212 <parkote9212@gmail.com>
2025-09-03 17:31:23 +0900

    java update

---
+++ src/ch07/sec03/exam02/SmartPhone.java
@@ -0,0 +1,9 @@
+package ch07.sec03.exam02;
+
+public class SmartPhone extends Phone {
+
+    public SmartPhone(String model, String color) {
+        super(model, color);
+        System.out.println("매개변수가진 스마트폰 생성자 실행");
+    }
+}

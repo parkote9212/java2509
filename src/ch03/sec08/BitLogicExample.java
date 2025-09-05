33c6c8e4b20b1838e30c6d8e08d0e406bbf4ed1c
parkote9212 <parkote9212@gmail.com>
2025-09-05 17:33:05 +0900

    ch03 update

---
+++ src/ch03/sec08/BitLogicExample.java
@@ -0,0 +1,22 @@
+package ch03.sec08;
+
+public class BitLogicExample {
+    public static void main(String[] args) {
+        System.out.println("45 & 25 = " + (45 & 25));
+        System.out.println("45 | 25 = " + (45 | 25));
+        System.out.println("45 ^ 25 = " + (45 ^ 25));
+        System.out.println("~45 = " + (~45));
+
+        byte receieData = -120;
+
+        int unsignedInt1 = receieData & 255;
+        System.out.println(unsignedInt1);
+
+        int unsignedInt2 =Byte.toUnsignedInt(receieData);
+        System.out.println(unsignedInt2);
+
+        int test = 136;
+        byte btest = (byte) test;
+        System.out.println(btest);
+    }
+}
